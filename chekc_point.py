0dbccc8091886870a2dbc887a2aea9351ee01a3a
Fakhrul Hidayat <fakhrulhidayat33@gmail.com>
2025-08-02 19:14:04 +0700

    add system to help me focus only 1 task (so I can't be burnout)

---
+++ chekc_point.py
@@ -0,0 +1,41 @@
+import re
+
+with open("README.md") as f:
+    lines = f.readlines()
+print(type(lines))
+head = None
+
+txt = "The rain in Spain"
+x = re.search("^The.*Spain$", txt)
+
+for line in lines:
+    if re.search(r"^## \*\*Stage", line):
+        head = line
+    elif re.search(r"^?(\- \[ \])", line):
+        if head != None:
+            break
+print("You are in: ")
+print(head)
+print("Your current task is")
+print(line)
+
+# data = dir(re)
+# stoper = 1
+# for i in data:
+#     print(i)
+#     if stoper % 5 == 0:
+#         run = input()
+#         if run == "q":
+#             break
+#     stoper += 1
+
+# test = re.match(r"$## Stage \d: .*", "## **Stage 1: Fundamentals**")
+# print(test)
+
+# txt = "The rain in Spain"
+# x = re.search("^The.*Spain$", txt)
+
+# if x:
+#   print("YES! We have a match!")
+# else:
+#   print("No match")
